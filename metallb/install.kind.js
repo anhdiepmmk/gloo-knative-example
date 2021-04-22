e232248a053d3f94bbf0fd54cb48019fb144e8f5
Diep <n08ni.dieppn@gmail.com>
2021-04-22 23:57:31 +0700

    chore: delete return statement

--- metallb/install.kind.js
+++ metallb/install.kind.js
@@ -4,20 +4,15 @@ const { EOL } = require('os');
 
 const { execSync } = require('child_process');
 
+// Create the metallb namespace
 console.log(
   execSync(
-    'kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.6/manifests/namespace.yaml', 
+    'kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/master/manifests/namespace.yaml', 
     { encoding: 'utf-8' }
   )
 );
 
-console.log(
-  execSync(
-    'kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.6/manifests/metallb.yaml', 
-    { encoding: 'utf-8' }
-  )
-);
-
+// Create the memberlist secrets
 try {
   console.log(
     execSync(
@@ -33,21 +28,31 @@ try {
   );
 }
 
-// calc ip range
-const output = execSync("docker network inspect -f '{{.IPAM.Config}}' kind", { encoding: 'utf-8' });
-const octets = output.match(/\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}/).pop().split('.');
-const network = `${octets[0]}.${octets[1]}`;
-const iprange = `${network}.255.200-${network}.255.250`
+// Apply metallb manifest
+console.log(
+  execSync(
+    'kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/master/manifests/metallb.yaml', 
+    { encoding: 'utf-8' }
+  )
+);
 
-// read template file
-let yaml = fs.readFileSync('./k8s-metallb-config.templ', 'utf-8');
-yaml = yaml.replace('{{IP_RANGE}}', iprange);
+setTimeout(() => {
+  // calc ip range
+  const output = execSync("docker network inspect -f '{{.IPAM.Config}}' kind", { encoding: 'utf-8' });
+  const octets = output.match(/\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}/).pop().split('.');
+  const network = `${octets[0]}.${octets[1]}`;
+  const iprange = `${network}.255.200-${network}.255.250`
 
-// write config file within iprange
-fs.writeFileSync('./k8s-metallb-config.yaml', yaml);
+  // read template file
+  let yaml = fs.readFileSync('./k8s-metallb-config.templ', 'utf-8');
+  yaml = yaml.replace('{{IP_RANGE}}', iprange);
 
-// apply config
-console.log(execSync('kubectl apply -f ./k8s-metallb-config.yaml', { encoding: 'utf-8' }));
+  // write config file within iprange
+  fs.writeFileSync('./k8s-metallb-config.yaml', yaml);
 
-// remove
-fs.unlinkSync('./k8s-metallb-config.yaml');
+  // apply config
+  console.log(execSync('kubectl apply -f ./k8s-metallb-config.yaml', { encoding: 'utf-8' }));
+
+  // remove
+  fs.unlinkSync('./k8s-metallb-config.yaml');
+}, 3000);
